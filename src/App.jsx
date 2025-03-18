d09bf355c188c1af5c4b6e2b8c5f4e0c5e1e5281
Ethan <157554972+ethanp4@users.noreply.github.com>
2025-03-17 19:07:13 -0600

    update page and add test

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
 
   return (
     <>
+      <h1>TECH 2102 - Enterprise Computing</h1>
       <p>This is Ethan Pelletier's jenkins App</p>
     </>
   )
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,8 +2,13 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 describe('App', () => {
-  it('renders the app component', () => {
+  it('displays exact text in p tag', () => {
     render(<App />);
     expect(screen.getByText(/This is Ethan Pelletier's jenkins App/i)).toBeInTheDocument();
   });
+  it("displays exact text in h1 tag", () => {
+    render(<App />);
+    expect(screen.getByText(/TECH 2102 - Enterprise Computing/i)).toBeInTheDocument();
+  });
 });
+
